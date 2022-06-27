7c58db9f277f32cb33609a4fc628118eddbdb683
nurmatthias <github@engleien.info>
2022-06-27 21:04:58 +0200

    Better map
    Creating random walltiles over the map and makinf them solid

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{Rltk, GameState, RGB, VirtualKeyCode};
+use rltk::{Rltk, GameState, RGB, VirtualKeyCode, Tile};
 use specs::prelude::*;
 use std::cmp::{max, min};
 use specs_derive::Component;
@@ -21,6 +21,12 @@ struct Renderable {
 struct Player {}
 
 
+#[derive(PartialEq, Clone, Copy)]
+enum TileType {
+    Wall, Floor
+}
+
+
 struct State {
     ecs: World,
 }
@@ -31,6 +37,10 @@ impl GameState for State {
         player_input(self, ctx);
 
         self.run_systems();
+
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -49,13 +59,18 @@ impl State {
 
 
 
-fn try_move_player(delta_x: i32, delty_y: i32, ecs: &mut World) {
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType >>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delty_y));
+        let destination_idx = xy_idx(pos.x+delta_x, pos.y+delta_y);
+
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 
 }
@@ -74,6 +89,60 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    return map;
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut x = 0;
+    let mut y = 0;
+
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0., 1.0, 0.), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+
+}
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
@@ -98,6 +167,8 @@ fn main() -> rltk::BError {
         })
         .with(Player{})
         .build();
+
+    gs.ecs.insert(new_map());
 
     rltk::main_loop(context, gs)
 }
