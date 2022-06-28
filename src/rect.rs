b8253714135ed5772ad63d20c5395f2fc1f0f176
nurmatthias <github@engleien.info>
2022-06-28 21:40:18 +0200

    New maprandomizer
    Implemented rooms and randomized placement of rooms

--- src/rect.rs
+++ src/rect.rs
@@ -12,7 +12,7 @@ impl Rect {
         Rect{x1:x, y1:y, x2:x+w, y2:y+h}
     }
 
-    pub fn intersect(&self, other: Rect) -> bool {
+    pub fn intersect(&self, other: &Rect) -> bool {
         self.x1 <= other.x2 && self.x2 >= other.x1 && self.y1 <= other.y2 && self.y2 >= other.y1
     }
 
